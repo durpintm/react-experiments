720ddde68862d9751e62810040c55ad6cbd54c96
durpintm <durpinthapa@gmail.com>
2024-05-18 20:57:57 -0400

    React wrapper component

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,61 @@
-import { useState } from "react";
+// import { useState } from "react";
 import "./App.css";
 
-function App() {
-  // When title which is a state variable changes the component re-renders
-  // Which means App re-renders and all the Header components used inside App re-renders even if one Header component state variable changes
-  // Solution is to push the state down, which means if there is a need to change the title of the header based on button click, push the state into the component that needs to re-render when it changes, then everything else does not re-renders
+// function App() {
+// When title which is a state variable changes the component re-renders
+// Which means App re-renders and all the Header components used inside App re-renders even if one Header component state variable changes
+// Solution is to push the state down, which means if there is a need to change the title of the header based on button click, push the state into the component that needs to re-render when it changes, then everything else does not re-renders
 
-  const updateTitle = () => {
-    setTitle(Math.random());
-  };
+//   return (
+//     <div>
+//       <HeaderWithButton title="Title 2" />
+//       <Header title="Title 3" />
+//       <Header title="Title 4" />
+//     </div>
+//   );
+// }
+
+// function HeaderWithButton() {
+//   const [title, setTitle] = useState("This is a title");
+//   const updateTitle = () => {
+//     setTitle("The title is " + Math.random());
+//   };
+//   return (
+//     <div>
+//       <button onClick={updateTitle}>Click to update</button>
+//       <Header title={title} />
+//     </div>
+//   );
+// }
+
+// // eslint-disable-next-line react/prop-types
+// function Header({ title }) {
+//   return (
+//     <>
+//       <div>{title}</div>
+//     </>
+//   );
+// }
+
+// Wrapper Component
+const App = () => {
   return (
     <div>
-      <HeaderWithButton title="Title 2" />
-      <Header title="Title 3" />
-      <Header title="Title 4" />
+      <CardWrapper innerComponent={<TextComponent />} />
     </div>
   );
+};
+
+function TextComponent() {
+  return <div>Hi text</div>;
 }
 
-function HeaderWithButton() {
-  const [title, setTitle] = useState("This is a title");
-  const updateTitle = () => {
-    setTitle("The title is " + Math.random());
-  };
+// eslint-disable-next-line react/prop-types
+function CardWrapper({ innerComponent }) {
   return (
     <div>
-      <button onClick={updateTitle}>Click to update</button>
-      <Header title={title} />
+      <div style={{ border: "4px solid black" }}>{innerComponent}</div>
     </div>
-  );
-}
-
-function Header({ title }) {
-  return (
-    <>
-      <div>{title}</div>
-    </>
   );
 }
 
